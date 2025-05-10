7f5093aff58cfe1b2240a4d5665e6ad2341c8f71
themisael100 <misalbryan@gmail.com>
2025-05-10 15:49:30 -0600

    java

---
+++ demo-SR23022/src/main/java/com/SR23022/App.java
@@ -0,0 +1,15 @@
+package com.SR23022;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public String Hello() {
+    	return "Hello World";
+    }
+    
+    public static void main( String[] args ) {
+    	App hw = new App();
+    	System.out.println(hw.Hello() );
+    }
+}
---
+++ demo-SR23022/src/test/java/com/SR23022/AppTest.java
@@ -0,0 +1,22 @@
+package com.SR23022;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
+
+/**	
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
